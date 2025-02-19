4b2fc7f97aa3fc0744e1a3c22414a3d3d4ad976a
SJH-706 <sjh-706@SJH-706-13.local>
2025-02-19 09:55:35 +0800

    Initial commit

---
+++ src/app/reactive-validation/reactive-validation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-validation',
+  standalone: true,
+  imports: [],
+  templateUrl: './reactive-validation.component.html',
+  styleUrl: './reactive-validation.component.css'
+})
+export class ReactiveValidationComponent {
+
+}
